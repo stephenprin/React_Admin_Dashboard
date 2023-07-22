bb7195ba2edc46e2c70f3a5870a3d1cda8b9ee92
Prince Nmezi <stephenprince427@gmail.com>
2023-07-22 02:33:00 +0100

    Side render

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom"
 import "./menu.scss"
+import {menu } from '../../utils/data'
 
 const Menu = () => {
   return (
-    <div>
-      Menu
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{ item.title}</span>
+        
+          {item.listItems.map((list) => (
+             <Link to={list.url} className="listItem" key={list.id}>
+             <img src={list.icon} alt="home" />
+              <span className="listItemTitle">{list.title }</span>
+           </Link>
+        ))}
+        
+
+      </div>
+    ))}
+
+    
     </div>
   )
 }
