3ff409792b5dba122d873329e5dab467fb03d8bd
Prince Nmezi <stephenprince427@gmail.com>
2023-07-23 10:08:06 +0100

    user information

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,18 @@
+import TopBox from '../../components/topBox/TopBox'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className="home">
       
-      <div className='box box1'>box1</div>
-      <div className='box box2'>box2</div>
-      <div className='box box3'>box3</div>
+      <div className='box box1'>
+        <TopBox/>
+      </div>
+      <div className='box box2 chartBox'>box2</div>
+      <div className='box box3 chartBox'>box3</div>
       <div className='box box4'>box4</div>
-      <div className='box box5'>box5</div>
-      <div className='box box6'>box6</div>
+      <div className='box box5 chartBox'>box5</div>
+      <div className='box box6 chartBox'>box6</div>
       <div className='box box7'>box7</div>
       <div className='box box8'>box8</div>
       <div className='box box9'>box9</div>
